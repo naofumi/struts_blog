752654e7a842b3b4b0a272ec33ff9bf403d16bb7
Naofumi Kagami <naofumi@mac.com>
2023-06-19 22:34:12 +0900

    AuthenticationMockable should not access database

--- src/test/java/struts_blog/setup/AuthenticationMockable.java
+++ src/test/java/struts_blog/setup/AuthenticationMockable.java
@@ -3,24 +3,35 @@ package struts_blog.setup;
 import org.mockito.Mockito;
 import struts_blog.daos.UserDao;
 import struts_blog.models.AuthenticationService;
+import struts_blog.models.User;
 
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
-
+/*
+* Usage
+*
+* action.setAuthenticationService(mockedAuthenticationServiceForEmail("test@example.com"));
+*  */
 public interface AuthenticationMockable {
+    // Creates a mock with a currentUser with an email that is as specified
     default AuthenticationService mockedAuthenticationServiceForEmail(String email) {
-        UserDao userDao = new UserDao();
+        User user = new User();
+        user.setEmail(email);
+
         AuthenticationService authServiceMock = Mockito.mock(AuthenticationService.class);
         Mockito.when(authServiceMock.userFromSession(any(Map.class)))
-                .thenReturn(userDao.findBy("email", email));
+                .thenReturn(user);
+
         return authServiceMock;
     }
 
+    // Creates a mock with no currentUser (unauthenticated)
     default AuthenticationService mockedAuthenticationServiceUnauthenticated() {
         AuthenticationService authServiceMock = Mockito.mock(AuthenticationService.class);
         Mockito.when(authServiceMock.userFromSession(any(Map.class)))
                 .thenReturn(null);
+
         return authServiceMock;
     }
 }
