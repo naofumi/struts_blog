e2ee07d934f1999d544312bdb0a3261f678f5a0a
Naofumi Kagami <naofumi@mac.com>
2023-06-19 22:35:10 +0900

    improve tests

--- src/test/java/struts_blog/actions/admin/posts/CreateActionWithMockTest.java
+++ src/test/java/struts_blog/actions/admin/posts/CreateActionWithMockTest.java
@@ -5,44 +5,61 @@ import junit.framework.TestCase;
 import org.mockito.Mockito;
 import struts_blog.actions.UnauthenticatedException;
 import struts_blog.daos.PostDao;
+import struts_blog.models.Authenticable;
 import struts_blog.models.Post;
+import struts_blog.setup.AuthenticationMockable;
 
 import java.util.HashMap;
 
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.verify;
 
-public class CreateActionWithMockTest extends TestCase {
+public class CreateActionWithMockTest extends TestCase implements AuthenticationMockable {
 
     public void setUp() throws Exception {
         super.setUp();
     }
 
+    public void test_fails_if_unauthenticated() {
+        CreateAction action = new CreateAction();
+
+        action.setAuthenticationService(mockedAuthenticationServiceUnauthenticated());
+
+        Exception exception = assertThrows(UnauthenticatedException.class, () -> {
+            action.execute();
+        });
+    }
+
     public void test_execute_returns_success() throws UnauthenticatedException {
-        PostDao postDaoMock = Mockito.mock(PostDao.class);
-        CreateAction action = new CreateAction(postDaoMock);
-
-        action.withSession(getSessionWithLoggedInUserId(1));
-
+        CreateAction action = new CreateAction();
         Post post = createPost("Test title", "Test content");
+
+        PostDao postDaoMock = Mockito.mock(PostDao.class);
         Mockito.when(postDaoMock.create(post)).thenReturn(true);
+        action.setPostDao(postDaoMock);
+
+        action.setAuthenticationService(mockedAuthenticationServiceForEmail("test@example.com"));
 
         action.setPost(post);
+
         String result = action.execute();
 
         assertEquals(ActionSupport.SUCCESS, result);
-        verify(postDaoMock).createAndReturnSaved(post);
     }
 
     public void test_execute_creates_new_post() throws UnauthenticatedException {
-        PostDao postDaoMock = Mockito.mock(PostDao.class);
-        CreateAction action = new CreateAction(postDaoMock);
-
-        action.withSession(getSessionWithLoggedInUserId(1));
-
+        CreateAction action = new CreateAction();
         Post post = createPost("Test title", "Test content");
+
+        PostDao postDaoMock = Mockito.mock(PostDao.class);
         Mockito.when(postDaoMock.create(post)).thenReturn(true);
 
+        action.setPostDao(postDaoMock);
+
+        action.setAuthenticationService(mockedAuthenticationServiceForEmail("test@example.com"));
+
         action.setPost(post);
+
         action.execute();
 
         verify(postDaoMock).createAndReturnSaved(post);
@@ -53,11 +70,5 @@ public class CreateActionWithMockTest extends TestCase {
         post.setTitle(title);
         post.setContent(content);
         return post;
-    }
-
-    private HashMap<String, Object> getSessionWithLoggedInUserId(int id) {
-        HashMap<String, Object> mockSession = new HashMap<>();
-        mockSession.put("user_id", 1);
-        return mockSession;
     }
 }
