6cbcbb4d9ce5abb3cd98c09381dca7d515da3f7c
Naofumi Kagami <naofumi@mac.com>
2023-06-21 09:09:03 +0900

    some refactoring of admin/users/EditAtion tests

--- src/test/java/struts_blog/actions/admin/users/EditActionWithStrutsTest.java
+++ src/test/java/struts_blog/actions/admin/users/EditActionWithStrutsTest.java
@@ -3,12 +3,15 @@ package struts_blog.actions.admin.users;
 import com.opensymphony.xwork2.ActionProxy;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.junit.StrutsTestCase;
+import struts_blog.actions.UnauthenticatedException;
 import struts_blog.daos.UserDao;
 import struts_blog.models.User;
 import struts_blog.setup.TestSetup;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.Assert.assertThrows;
 
 /*
  * Here we are testing the capabilities of the `StrutsTestCase`.
@@ -18,12 +21,16 @@ import java.util.Map;
  * */
 public class EditActionWithStrutsTest extends StrutsTestCase {
     ActionProxy actionProxy;
+    EditAction action;
     UserDao userDao = new UserDao();
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
         new TestSetup().setUpDb();
+
+        this.actionProxy = getActionProxy("/admin/users/edit.action");
+        this.action = (EditAction) actionProxy.getAction();
     }
 
     @Override
@@ -33,16 +40,20 @@ public class EditActionWithStrutsTest extends StrutsTestCase {
     }
 
     public void test_path_routes_here() {
-        this.actionProxy = getActionProxy("/admin/users/edit.action");
+        assertTrue(action instanceof EditAction);
+    }
 
-        assertTrue(actionProxy.getAction() instanceof EditAction);
+    public void test_fails_if_unauthenticated() throws Exception {
+        action.setId(1);
+
+        String result = actionProxy.execute();
+        assertEquals("unauthenticated", result);
+        assertEquals(302, response.getStatus());
     }
 
     public void test_execute_returns_success_and_sets_getUser() throws Exception {
         User userToEdit = userDao.findBy("email", "spongebob@example.com");
 
-        this.actionProxy = getActionProxy("/admin/users/edit.action");
-        EditAction action = (EditAction) actionProxy.getAction();
         action.setId(userToEdit.getId());
 
         actionProxy.getInvocation().getInvocationContext().withSession(new HashMap<>(Map.of("user_id", 1)));
